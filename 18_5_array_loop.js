c1b392a2c484b4d73b0d19d81162c4233b2c42a7
ProdipKirtania <prodip.tukhor@gmail.com>
2023-08-01 23:28:40 +0600

    1st commit

---
+++ 18_5_array_loop.js
@@ -0,0 +1,12 @@
+
+var number = [15, 25, 35, 56, 85, 69, 87];
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for (var i=0; i <= 6; i++) {
+    console.log(i);
+    
+}
