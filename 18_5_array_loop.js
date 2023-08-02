76ceda84933d82d6294330d2f7a154a9f4123340
ProdipKirtania <prodip.tukhor@gmail.com>
2023-08-03 00:07:25 +0600

    practice problem 1

--- 18_5_array_loop.js
+++ 18_5_array_loop.js
@@ -1,12 +1,19 @@
 
-var number = [15, 25, 35, 56, 85, 69, 87];
+var numbers = [15, 25, 35, 56, 85, 69, 87];
 
 // for (let index = 0; index < array.length; index++) {
 //     const element = array[index];
     
 // }
 
-for (var i=0; i <= 6; i++) {
-    console.log(i);
+// for (var i=0; i <= 6; i++) {
+//     var number = numbers[i];
+//     console.log(number);
     
+// }
+
+var items = ['pen', 'pencil', 'mouse', 'keyboard', 'glass', 'mobile'];
+for (var i=0; i <= items.length; i++) {
+    var item = items[i];
+    console.log(item);
 }
---
+++ 18_8_practice_problem.js
@@ -0,0 +1,9 @@
+var moneyGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var totalCost = applePrice + orangePrice;
+console.log('Total cost: '+totalCost);
+
+var moneyLeft = moneyGiven - totalCost;
+console.log('Money Left: '+moneyLeft);
