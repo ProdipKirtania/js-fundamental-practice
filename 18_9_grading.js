17e308e66f71d4da22608faf60feebb5bcc79ea5
ProdipKirtania <prodip.tukhor@gmail.com>
2023-08-04 23:08:31 +0600

    grading practice problem solution

---
+++ 18_9_grading.js
@@ -0,0 +1,23 @@
+var myMarks = 85;
+var tomMarks = 66;
+var janeMarks = 95;
+var peterMarks = 56;
+var johnMarks = 40;
+
+var finalMarks = myMarks;
+
+if(finalMarks >= 80){
+    console.log('A Grade');
+}
+else if(finalMarks >= 60){
+    console.log('B Grade');
+}
+else if(finalMarks >= 50){
+    console.log('C Grade');
+}
+else if(finalMarks >= 40){
+    console.log('D Grade');
+}
+else{
+    console.log('F Grade');
+}
