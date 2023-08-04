71b9caea1b18ca06f620a813920ced29b1b9d561
ProdipKirtania <prodip.tukhor@gmail.com>
2023-08-04 22:58:17 +0600

    array update problem practiced

---
+++ 18_9_array_update.js
@@ -0,0 +1,22 @@
+// Probelm-01 // fruit list
+var fruits = ['Apple', 'Banana', 'Orange'];
+console.log(fruits);
+
+// indexOf Banana
+var bananaIndex = fruits.indexOf('Banana');
+console.log(bananaIndex);
+
+// replace banana with Mango
+fruits[1] = 'Mango';
+console.log(fruits);
+
+// remove orange
+fruits.pop();
+console.log(fruits);
+
+// add watermelon
+fruits.push('Watermelon');
+console.log(fruits);
+
+
+
