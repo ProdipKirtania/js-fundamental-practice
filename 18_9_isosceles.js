103513bba31f75a12682c26047203812d2a333be
ProdipKirtania <prodip.tukhor@gmail.com>
2023-08-04 23:54:42 +0600

    isosceles

---
+++ 18_9_isosceles.js
@@ -0,0 +1,10 @@
+var sideOne = 9;
+var sideTwo = 8;
+var sideThree = 8;
+
+if(sideOne == sideTwo || sideTwo == sideThree || sideOne == sideThree){
+    console.log('The triangle is isosceles');
+}
+else{
+    console.log('The triangle is not isosceles');
+}
